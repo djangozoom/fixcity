51925ed41606fde464596404aff52da4067b96cd
Nate Aune <natea@jazkarta.com>
2011-06-18 11:45:10 +0200

    commenting out the static serve stuff because these static assets will be served up by nginx. still need to figure out what to do about the uploads though.

--- fixcity/urls.py
+++ fixcity/urls.py
@@ -66,10 +66,10 @@ urlpatterns = patterns(
     # Static media for dev work.  For deployment, these should be served
     # by a front-end server eg. apache!
     # see http://docs.djangoproject.com/en/dev/howto/static-files/
-    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
-      {'document_root': settings.STATIC_DOC_ROOT, 'show_indexes': True}),
-    (r'^uploads/(?P<path>.*)$', 'django.views.static.serve',
-      {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+#    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+#      {'document_root': settings.STATIC_DOC_ROOT, 'show_indexes': True}),
+#    (r'^uploads/(?P<path>.*)$', 'django.views.static.serve',
+#      {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
